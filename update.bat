c7e5aa48be554de4558944f510c67b6c5f5dfdda
Maik <derbruedi@googlemail.com>
2025-08-31 11:24:59 +0200

    test

--- update.bat
+++ update.bat
@@ -1,8 +1,6 @@
 @echo off
 echo start Update...
-cd /d "%~dp0\mystavaria"
-set PATH=%CD%\..\PortableGit\cmd;%PATH%
-git fetch origin
-git reset --hard origin/main
-git clean -fd
+cd /d "%~dp0"
+set PATH=%CD%\PortableGit\cmd;%PATH%
+git pull origin main
 pause
